7b1e58262befd3fadaabaf89664c07871acdbc68
Dmitrii <andreanov1234@gmail.com>
2023-02-19 19:39:53 +1000

    Решение классной задачи: Двойная защита

---
+++ WEB. Templates. Flask-wtf/Classwork_task_5/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+# После запуска программы нужно перейти по ссылке: http://127.0.0.1:8080/login там будет лежать ответ на задачу
+
+app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    cap_name = StringField('id капитана', validators=[DataRequired()])
+    cap_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+    app.run(port=8080, host='127.0.0.1')
