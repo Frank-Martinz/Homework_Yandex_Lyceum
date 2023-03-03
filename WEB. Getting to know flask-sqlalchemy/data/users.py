711119ce422279110ff46261a3ff5ccb3b2811a5
Dmitrii <andreanov1234@gmail.com>
2023-03-03 21:21:14 +1000

    Решение классной задачи: Журнал работ

--- WEB. Getting to know flask-sqlalchemy/data/users.py
+++ WEB. Getting to know flask-sqlalchemy/data/users.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlalchemy
+from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 
 
@@ -17,3 +18,8 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
+
+    jobs = orm.relationship("Jobs", back_populates='user')
+
+    def __repr__(self):
+        return f"<Colonist> {self.id} {self.surname} {self.name}"
--- WEB. Getting to know flask-sqlalchemy/main.py
+++ WEB. Getting to know flask-sqlalchemy/main.py
@@ -1,50 +1,25 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template, redirect
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
+
+# После запуска программы переходим по ссылке: http://127.0.0.1:5000/ где и будет лежать ответ
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-# После запуска программы ищем в папке db файл blogs.db
-# В нём в таблице users будет лежать решение
 
 def main():
-    db_session.global_init("db/blogs.db")
-
-    user = User()
-    user.name = 'Ridley'
-    user.surname = 'Scott'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
-
-    user1 = User()
-    user1.name = 'Vasya'
-    user1.surname = 'Petrov'
-    user1.age = 44
-    user1.position = 'captain helper'
-    user1.speciality = 'research engineer'
-    user1.address = 'module_2'
-    user1.email = 'vasya_petrov@mars.org'
-
-    user2 = User()
-    user2.name = 'Oleg'
-    user2.surname = 'Tinkov'
-    user2.age = 55
-    user2.position = 'investor'
-    user2.speciality = 'bank_manager'
-    user2.address = 'module_1'
-    user2.email = 'olega_tinkov@gmail.com'
-
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.add(user1)
-    db_sess.add(user2)
-    db_sess.commit()
-
     app.run()
+
+
+@app.route('/')
+@app.route('/answer')
+def tablet():
+    db_session.global_init('db/blogs.db')
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs)
+    return render_template('tablets.html', job=job)
 
 
 if __name__ == '__main__':
